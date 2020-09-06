4394ba69aeaab07af7423d5510feeb04ed5035ae
Ademir Sehic <sehic.ademir@gmail.com>
2020-09-06 19:50:18 +0200

    Added token input route on navbar

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -36,14 +36,17 @@ class SearchBox extends Component {
                 <div className="col-xl-1 col-lg-2 col-md-4 col-4 mx-auto mx-lg-0 mb-3 mb-lg-0">
                     <Link to="/"><img src="/logo.png" className="img-fluid w-25" alt="logo" /></Link>
                 </div>
+                <div className="col-xl-3 col-lg-5 col-md-3 my-2 my-lg-0">
+                    <Link to="/insert/token"><button className="btn btn-light">No results? Enter your token here!</button></Link>
+                </div>
             <form className="col-lg-4 mx-auto mx-md-0 ml-md-auto offset-md-4 col-12 row text-center" >
-                <select onChange={this.handleChange} name="searchPrefix" className="form-control col-lg-4 col-12 mb-2 mb-md-0 text-capitalize mx-auto">
+                <select onChange={this.handleChange} name="searchPrefix" className="form-control col-xl-4 col-lg-3 col-12 mb-2 mb-md-0 text-capitalize mx-auto">
                     { searchPrefixOptions.map((searchPrefixOption) =>
                         <option key={searchPrefixOption} value={searchPrefixOption} >{searchPrefixOption}</option>
                     )}
                 </select>
-			    <input type="text" id="searchUser" className="form-control col-lg-5 col-12 ml-1 mx-auto my-2 my-lg-0"  placeholder={"Search " + searchPrefix} value={searchValue} name="search" onChange={this.handleChange} required />
-                <Link className="col-lg-2 col-12 ml-md-1 my-2 my-md-0 no-bg"  to={isValidSearch ? `/${searchPrefix}/${searchValue}/1` : ''}><button className="btn btn-light">Search</button></Link>
+			    <input type="text" id="searchUser" className="form-control col-xl-5 col-lg-4 col-12 ml-1 mx-auto my-2 my-lg-0"  placeholder={"Search " + searchPrefix} value={searchValue} name="search" onChange={this.handleChange} required />
+                <Link className="col-xl-2 col-lg-3 col-12 ml-md-1 my-2 my-md-0 no-bg"  to={isValidSearch ? `/${searchPrefix}/${searchValue}/1` : ''}><button className="btn btn-light">Search</button></Link>
 		    </form>
             </div>
          );
